87eb304a3ecf422af6e213ad6cd2f1712672cc8a
Adam Borowski <aborowski@atlassian.com>
2016-04-13 21:17:54 +0200

    changed default duration

--- standalone/client/js/features/musicfiles/service/PlaybackService.js
+++ standalone/client/js/features/musicfiles/service/PlaybackService.js
@@ -2,7 +2,7 @@ var Howl = require('howler').Howl;
 module.exports = class PlaybackService {
     constructor(utils, $rootScope, MusicFileService) {
         this.utils = utils;
-        this.defaultDuration = 10000;
+        this.defaultDuration = localStorage.get('default-duration') || 300000;
         this.fadeDuration = 2000;
         this.$rootScope = $rootScope;
         this._playFromStart = true;
